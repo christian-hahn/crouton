6dfeb591aeab411d311dd60685d77ece81788e41
Christian Hahn <christianhahn09@gmail.com>
2022-01-31 17:17:38 -0800

    test_server_client.py: expanded on TestObject test-case

--- tests/test_server_client.py
+++ tests/test_server_client.py
@@ -1,6 +1,6 @@
-"""Tests for Server object.
+"""Tests for Server & client object.
 
-This module contains unit-tests for the Server object.
+This module contains unit-tests for the Server & client object.
 """
 
 from crouton import Server, Client
@@ -15,20 +15,25 @@ PORT = 5002
 class ServerClientTestCase(unittest.TestCase):
 
     def setUp(self):
-        self._server = Server(host=HOST, port=PORT)
-        self._server_thread = Thread(target=self._server.run)
+        self._server = Server()
+        kwargs = {'host': HOST, 'port': PORT}
+        self._server_thread = Thread(target=self._server.run, kwargs=kwargs)
         self._server_thread.start()
-        self._server.wait_for()
+        self._server._wait_for()
         self._client = Client(host=HOST, port=PORT)
 
     def tearDown(self):
-        self._server.shutdown()
+        # Tell server to shutdown. Just for unit-testing.
+        self._server._shutdown()
         del self._client
+        # Connect once more to get the server to shutdown. This is just a
+        # hack for unit-test flow, since server is blocking on accept().
+        cli = Client(host=HOST, port=PORT)
+        del cli
         self._server_thread.join()
 
     def test_list(self):
-        self._server.register(list)
-        obj = self._client.factory('list')
+        self._server.register_type(list)
         obj = self._client.factory(list)
         cases = (
             1.1,
@@ -46,25 +51,35 @@ class ServerClientTestCase(unittest.TestCase):
             self.assertTrue(len(obj))
         for method in (str, repr, format, bool):
             method(obj)
+        self.assertFalse('abc' in obj)
 
     def test_dict(self):
-        self._server.register(list)
+        self._server.register_type(list)
         obj = self._client.factory('list')
-        obj = self._client.factory(list)
         print(len(obj))
         obj.append('field1')
 
     def test_obj(self):
-
-        class TestObj(object):
-            def __init__(self, arg1, arg2, kwarg1=None):
-                self.arg1 = arg1
-                self.arg2 = arg2
-                self.kwarg1 = kwarg1
-
-        self._server.register(TestObj)
-        obj = self._client.factory(TestObj, 'arg1', 'arg2')
+        self._server.register_type(TestObject)
+        obj = self._client.factory('TestObject', 'first arg', kwarg1='a keyword arg')
         print(obj.arg1)
+        with obj as obj1:
+            print(obj1)
+
+
+class TestObject:
+
+    def __init__(self, arg1, kwarg1=None):
+        self.arg1 = arg1
+        self.kwarg1 = kwarg1
+
+    def __enter__(self):
+        print('in __enter__()')
+        return self
+
+    def __exit__(self, type, value, traceback):
+        print('in __exit__()')
+
 
 if __name__ == '__main__':
     unittest.main()
